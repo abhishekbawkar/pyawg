36223b37f398b9b9ee643c1a2f48e8737f54515a
Abhishek Bawkar <abhishekbawkar@gmail.com>
2025-03-21 08:50:32 +0000

    Fixed Import issue caused by commenting a couple of imports

--- pyawg/__init__.py
+++ pyawg/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # from .base import AWG
-# from .rigol import RigolDG1000Z
-# from .siglent import SiglentSDG1000X
+from .rigol import RigolDG1000Z
+from .siglent import SiglentSDG1000X
 from .enums import *
 from .awg_control import awg_control
 from .utils import setup_logging
